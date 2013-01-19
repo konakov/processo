eac92ba248fd5598210370f08968281bc4a65931
olya-d <inremajor@gmail.com>
2013-01-19 22:54:49 +0400

    Nested edit form for document

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,7 +1,9 @@
 class Connection < ActiveRecord::Base
 
-  attr_accessible :operation, :inoutput, :way
+  attr_accessible :operation, :inoutput, :way, :inoutput_attributes
 
   belongs_to :operation
   belongs_to :inoutput
+
+  accepts_nested_attributes_for :inoutput
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,8 +1,10 @@
 class Operation < ActiveRecord::Base
-  attr_accessible :document_id, :exceptions, :goal, :name, :steps, :worker
+  attr_accessible :document_id, :exceptions, :goal, :name, :steps, :worker, :connections, :connections_attributes
 
   belongs_to :document
 
   has_many :connections
   has_many :inoutputs, through: :connections
+
+  accepts_nested_attributes_for :connections
 end
