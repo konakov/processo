c300f4296b3a967a1de8a8805df0d7610e781921
olya-d <inremajor@gmail.com>
2013-01-19 18:47:43 +0400

    Root to documents#index, public.html removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Processo::Application.routes.draw do
   resources :documents
 
+  root to: "documents#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
