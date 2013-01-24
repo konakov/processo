d31ed14d60cf7885429c6c31c58663d56ec7042e
olya-d <inremajor@gmail.com>
2013-01-24 16:17:01 +0400

    Inoutput model test

--- spec/models/inoutput_spec.rb
+++ spec/models/inoutput_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Inoutput do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @inoutput = FactoryGirl.create :inoutput }
+
+  it { should respond_to :connections }
+  it { should respond_to :operations }
+
+  describe "with invalid attributes" do
+    after(:each) do
+      it { should_not be_valid }
+    end
+
+    describe "without body" do
+      before { @inoutput.body = "" }
+    end
+  end
 end
