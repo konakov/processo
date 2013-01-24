0e9526d6e3c7e0deb5c5d50520ad0566b6bcbd3f
olya-d <inremajor@gmail.com>
2013-01-24 11:56:04 +0400

    Document model tests

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,6 @@
 class Document < ActiveRecord::Base
   attr_accessible :description, :operation_id, :metrics, :name, :purpose, :operations_attributes
-  has_many :operations
+  has_many :operations, dependent: :destroy
 
   validates :description, presence: true
   validates :metrics, presence: true
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,5 +1,57 @@
 require 'spec_helper'
 
 describe Document do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @document = FactoryGirl.create :document
+  end
+
+  subject { @document }
+
+  it { should respond_to :name }
+  it { should respond_to :description }
+  it { should respond_to :purpose }
+  it { should respond_to :metrics }
+  it { should respond_to :operations }
+
+  describe "with invalid attributes" do
+
+    after(:each) do
+      it { should_not be_valid }
+    end
+
+    describe "with empty name" do
+      before { @document.name = "" }
+    end
+
+    describe "without purpose" do
+      before { @document.purpose = "" }
+    end
+
+    describe "with empty description" do
+      before { @document.description = "" }
+    end
+
+    describe "with empty metrics" do
+      before { @document.metrics = "" }
+    end
+
+  end
+
+  describe "the same name twice" do
+    before do
+      @another_document = FactoryGirl.build :document
+      @another_document.name = @document.name
+    end
+
+    subject { @another_document }
+
+    it { should_not be_valid }
+  end
+
+  describe "when destroyed" do
+    it "should destroy operation" do
+      @operation = FactoryGirl.create :operation, document: @document
+      expect { @document.destroy }.to change(Operation, :count).by(-1)
+    end
+  end
 end
